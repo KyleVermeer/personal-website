d1393c672361604f3fe3a7f535e13a68d397a998
Kyle Vermeer <kvermeer@box.com>
2014-12-28 16:45:00 -0800

    Prototype minus photos

--- routes.js
+++ routes.js
@@ -4,4 +4,6 @@ module.exports = function(app) {
 
     app.get('/', main.index);
     app.get('/contact', main.contact);
+    app.get('/aboutMe', main.aboutMe);
+    app.get('/projects', main.projects);
 }
