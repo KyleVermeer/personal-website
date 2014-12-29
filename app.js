77c3a79a56d6734ecfc2bd6eaa5299b17fdcc7bb
Kyle Vermeer <kvermeer@box.com>
2014-12-28 16:45:00 -0800

    Prototype minus photos

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
-var express = require('express')
-var app = express()
+var express = require('express');
+var app = express();
+var expressLayouts = require('express-ejs-layouts');
 
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
+app.use(expressLayouts);
 
 app.set('views', './views');
 app.engine('ejs', require('ejs').__express);
 app.set('view engine', 'ejs');
+app.set('layout', 'main-layout');
 
 var routes = require("./routes.js")(app);
 
@@ -15,6 +18,6 @@ var server = app.listen(app.get('port'), function () {
   var host = server.address().address
   var port = server.address().port
 
-  console.log('App listening at http://%s:%s', host, port)
+  console.log('Example app listening at http://%s:%s', host, port)
 
 });
--- controllers/main.js
+++ controllers/main.js
@@ -1,7 +1,22 @@
+var MILLISECONDS_IN_YEAR = 1000 * 60 * 60 * 24 * 365;
+
 module.exports.index = function(req, res) {
     res.render('index', { title: 'KV' } );
 }
 
 module.exports.contact = function(req, res) {
-    res.render('contact', { title: 'Contact Me'} );
+    res.render('contact', { title: 'Contact Kyle'} );
+}
+
+module.exports.aboutMe = function(req, res) {
+    var birthday = new Date(1991, 3, 2);
+    var now = Date.now();
+    var elapsedTime = now - birthday;
+    var yearsOld = Math.floor(elapsedTime / MILLISECONDS_IN_YEAR);
+    var data = { title: 'About Kyle', yearsOld : yearsOld };
+    res.render('aboutMe', data);
+}
+
+module.exports.projects = function(req, res) {
+    res.render('projects', { title: 'Projects'} );
 }
